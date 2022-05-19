dd3181f1a792cdd8b07ef7e4d3c9d772d2c2b884
VinceRF <zhsk30@naver.com>
2022-05-19 17:23:31 +0900

    4번 택시 생성으로 실행 예 추가

--- homework/Taxi.java
+++ homework/Taxi.java
@@ -14,9 +14,20 @@ public class Taxi {
         t2.StartDrive(11);
         t2.showinfo();
 
+
         Build_Taxi t3 = new Build_Taxi();
-        t3.StartDrive(9);
+        t3.StartDrive(8);
         t3.showinfo();
+
+        Build_Taxi t4 = new Build_Taxi();
+        t4.Destination("광화문");
+        t4.StartDrive(11);
+//        t4.take(); // 기름이 충분하고 기본요금, 목적지 등의 값이 있더라도 take()로 승객을 태우지 않으면 일반 상태
+        t4.Speed(50);
+        t4.fee(1,2,3300,200);
+        t4.showinfo();
+
+
 
     }
 }
